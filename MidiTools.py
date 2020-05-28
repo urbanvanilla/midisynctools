92d8d0bbd28e6306f392a338ccfcb08a57aa4a59
urbanvanilla <jamesteow@gmail.com>
2020-05-28 13:06:17 -0400

    initial commit

---
+++ MidiTools.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+from guizero import App, Text, PushButton, yesno, info
+import subprocess
+
+def startBT():
+    welcome_message.value = "Starting Bluetooth MIDI Server"
+    subprocess.Popen(["xterm", "./ToolScripts/btMidi"])
+
+def checkStatus():
+    welcome_message.value = "Displaying MIDI Connections"
+    subprocess.Popen(["xterm","./ToolScripts/MidiStatus"])
+
+def startMidiConnector():
+    welcome_message.value = "Starting Midi Connector"
+    subprocess.Popen(["./ToolScripts/MidiConnectorPy.py"])
+
+    
+def startAPReciever():
+    welcome_message.value = "Starting AirPlay Reciever"
+    info("Info", "Best run when in 1080p HDMI output. May need to run '/home/pi/LCD-show/LCD-hdmi' prior to running.")
+    if (yesno("RPiPlay", "Display may lose visual feedback, best used with keyboard/VNC control. Terminate with Ctrl+ C. Continue?")):
+        subprocess.Popen(["xterm", "-e", "/home/pi/RPiPlay/build/rpiplay", "-n","MidiToolsAP", "-b", "off"])
+
+def startLCDhdmi():
+    welcome_message.value = "Changing display settings"
+    if(yesno("Reboot?", "This will require a restart. Continue?")):
+        subprocess.Popen(["xterm", "-e", "sudo", "/home/pi/LCD-show/LCD-hdmi"], cwd="/home/pi/LCD-show")
+
+def startLCDdriver():
+    welcome_message.value = "Changing display settings"
+    if(yesno("Reboot?", "This will require a restart. Continue?")):
+        subprocess.Popen(["xterm", "-e", "sudo", "/home/pi/LCD-show/MPI4008-show"], cwd="/home/pi/LCD-show")
+    
+    
+app = App(title="Midi Sync Tools", layout="auto", height="350", width="300")
+
+welcome_message = Text(app, text=" ", size=12, font="Helvetica", color="Black")
+
+
+BTbutton = PushButton(app, command=startBT, text="Start Bluetooth MIDI", width="fill")
+
+MidiStatbutton = PushButton(app, command=checkStatus, text="Check MIDI Status", width="fill")
+
+MidiConnectbutton = PushButton(app, command=startMidiConnector, text="Midi Connector", width="fill")
+
+
+APbutton = PushButton(app, command=startAPReciever, text="Start AirPlay Reciever", width="fill")
+
+LCDhdmibutton = PushButton(app, command=startLCDhdmi, text="Monitor Display", width="fill")
+
+MPI4button = PushButton(app, command=startLCDdriver, text="Touch Display", width="fill")
+
+Output_message = Text(app, text="by James", size=10, font="Helvetica", color="Black", align = "bottom")
+
+
+app.display()
---
+++ ToolScripts/MidiConnectorPy.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+from guizero import App, ButtonGroup, Text, PushButton, Box, info
+import subprocess
+import re
+
+sourceid = 0
+destinationid = 0
+
+def changeSource():
+    sourceid = midisource.value
+
+def changeDestination():
+    destinationid = mididestination.value
+    
+def checkStatus():
+    subprocess.Popen(["uxterm","/home/pi/James/Scripts/MidiStatus"])
+    
+def connectMidi():
+    sourceid = midisource.value
+    destinationid = mididestination.value
+    subprocess.Popen(["aconnect", str(sourceid)+":0", str(destinationid)+":0"])
+    
+def disconnectMidi():
+    subprocess.Popen(["uxterm", "-e", "aconnect", "-x"])
+
+cmd = [ "aconnect", "-l"]
+rawmidilist = str(subprocess.Popen( cmd, stdout=subprocess.PIPE ).communicate()[0])
+pattern1 = r"client (\d+)"
+clients = re.findall(pattern1, rawmidilist)
+pattern2 = r": '(.{15})"
+names = re.findall(pattern2, rawmidilist)
+
+for y in range(0,len(names)):
+    temp = names[y].split("'")
+    names[y] = temp[0]
+
+
+app = App(title="Midi Connector", layout="grid", height='300', width='400')
+
+TopBox = Box(app, align='top', height='100', width='200', grid=[1,1])
+LeftBox = Box(app, align='left', grid=[0,3])
+
+RightBox = Box(app, align='right', grid=[3,3])
+BottomBox = Box(app, align='bottom', height='100', width='300', grid=[1,3])
+
+MidiStatbutton = PushButton(TopBox, command=checkStatus, text="Check MIDI Status", align='top', width="fill")
+sourcetitle = Text(LeftBox, text="Source", size=12, font="Helvetica", color="Black", align="top")
+destinationtitle = Text(RightBox, text="Destination", size=12, font="Helvetica", color="Black", align="top")
+ConnectButton = PushButton(BottomBox, text="Connect", command=connectMidi, width="fill")
+DisconnectButton = PushButton(BottomBox, text="Disconnect All", command=disconnectMidi, width="fill")
+
+midisource = ButtonGroup(LeftBox, command=changeSource, align="left")
+mididestination = ButtonGroup(RightBox, command=changeDestination, align="right")
+
+for x in range(0,len(clients)):
+    row = []
+    row.append(names[x])
+    row.append(clients[x])
+    midisource.append(row)
+    
+for x in range(0,len(clients)):
+    row = []
+    row.append(names[x])
+    row.append(clients[x])
+    mididestination.append(row)
+    
+app.display()
+
+
+
